f21625b03977297311fda7fabdac3084a92c5e0b
mochrks <mochrizkiks@gmail.com>
2024-11-15 10:37:58 +0700

    feat: github activity calendar

--- src/components/demo/RecentProject.tsx
+++ src/components/demo/RecentProject.tsx
@@ -3,7 +3,7 @@ import ShimmerButton from "@/components/magicui/shimmer-button";
 import { useNavigate } from "react-router-dom";
 import { CardProject } from "@/components/demo/CardProject";
 import { recentProjects } from "@/apis/project";
-
+import GitHubCalendar from 'react-github-calendar';
 export function RecentProject() {
   const navigate = useNavigate();
 
@@ -13,6 +13,12 @@ export function RecentProject() {
 
   return (
     <>
+      <div className="hidden md:flex flex-col w-full items-center justify-center mt-20 ">
+        <h4 className="scroll-m-20 text-xl  tracking-tight py-5">
+          @mochrks on GitHub
+        </h4>
+        <GitHubCalendar username="mochrks" fontSize={16} colorScheme="dark" blockRadius={13} maxLevel={9} />
+      </div>
       <div className="relative flex w-full h-full flex-col items-center overflow-hidden justify-center gap-10 pt-20 ">
         <div className="container flex items-center justify-center ">
           <div className="max-w-full mx-auto ">
